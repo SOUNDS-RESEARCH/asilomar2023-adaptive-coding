da73b2c4b298a233a42fba0f0c04bcb3dde9b185
Matthias Blochberger <matthias.blochberger@esat.kuleuven.be>
2023-07-24 12:52:23 +0200

    some improvements

--- dockerfile
+++ dockerfile
@@ -1,8 +1,9 @@
-FROM python:3.10-slim
+FROM python:3.11-slim
 # RUN apt-get update && apt-get install -y git
 COPY . .
 RUN pip install -r src/requirements.txt
 WORKDIR /wd
 ENV PYTHONPATH "${PYTHONPATH}:/wd"
 ENTRYPOINT ["python", "src/simulate.py"]
-CMD ["100", "4"]
+# default parameters for simulate.py: [runs, seed, num_processes]
+CMD ["100", "12345", "4"]
--- src/simulate.py
+++ src/simulate.py
@@ -7,28 +7,42 @@ import polars as pl
 # from pythonutils import utils
 from pythonutils import dockersim
 
+# The parameters of this script, the number of relizations per parameter combination,
+# the seed of the random number generator, the number of processes to be used on parallel
 runs = int(sys.argv[1])
-nprocesses = int(sys.argv[2])
+seed = int(sys.argv[2])
+num_processes = int(sys.argv[3])
 
 
+# This is the actual simulation function. We simulate the ADMM algorithm
+# (imported from admm_fq_*) for different algorithm onfigurations and parameter combinations.
 def simulate(a, b, rng=np.random.default_rng()):
     data = rng.normal(loc=a, scale=b, size=(6000,))
     # time.sleep(0.4)
-    return data
+    for val in data:
+        yield {"val": val, "sq_val": val * val}
 
 
+# This has to match the dictionary keys that simulate yields
+return_value_names = ["val", "sq_val"]
+
+# Define the tasks, which are basically all the parameter combinations for
+# which the algorithms are supposed to be run
 tasks = [{"a": a, "b": b} for a in range(1, 10) for b in range(1, 10)]
-sim = dockersim.DockerSim(simulate, tasks, 555, datadir="data")
-print(f"Running {runs} of {len(tasks)} tasks each in {nprocesses} processes.")
-sim.run(runs=runs, num_processes=nprocesses)
 
+# Create and run the simulation
+sim = dockersim.DockerSim(simulate, tasks, return_value_names, seed, datadir="data")
+sim.run(runs=runs, num_processes=num_processes)
+
+# Read the data from the resulting csv files (we are using polars lazy api)
 df = pl.scan_csv("data/results_*.csv")
 
+# Plot and save relevant figures
 fig = plt.figure()
 sns.lineplot(
     df.filter(pl.col("b") == 5).collect(),
     x="series",
-    y="value",
+    y="val",
     hue="a",
     errorbar=("sd", 1.96),
 )
@@ -37,3 +51,17 @@ plt.ylabel("Value")
 plt.title("Random value")
 
 fig.savefig("plots/figure.png")
+
+fig = plt.figure()
+sns.lineplot(
+    df.filter(pl.col("b") == 5).collect(),
+    x="series",
+    y="sq_val",
+    hue="a",
+    errorbar=("sd", 1.96),
+)
+plt.xlabel("Series")
+plt.ylabel("Value")
+plt.title("Random value")
+
+fig.savefig("plots/figure1.png")
